7e7d6163a92768dd81a009fd6b3fa41c79bcb896
benoist <benoist.claassen@gmail.com>
2013-01-13 18:03:48 +0100

    minor refactor

--- spec/lib/xmldsig/transforms/transform_spec.rb
+++ spec/lib/xmldsig/transforms/transform_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Xmldsig::Transforms::Transform do
 
   it "raises a warning when transform is called" do
-    Xmldsig::Transforms::Transform.any_instance.should_receive(:warn)
+    described_class.any_instance.should_receive(:warn)
     described_class.new(nil,nil).transform
   end
 
