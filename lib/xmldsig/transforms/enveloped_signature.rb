5078e68def48af5869b41af7370fb09c889c2765
Gabriel Teles <gab.teles@hotmail.com>
2017-02-15 13:43:30 -0200

    [fix] null check

--- lib/xmldsig/transforms/enveloped_signature.rb
+++ lib/xmldsig/transforms/enveloped_signature.rb
@@ -5,7 +5,7 @@ module Xmldsig
         signatures = node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).
             sort { |left, right| left.ancestors.size <=> right.ancestors.size }
 
-        signatures.first.remove
+        signatures.first.remove if signatures.first
         node
       end
     end
