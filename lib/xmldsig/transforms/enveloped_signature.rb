6b35e90a655318fd7e6abcf455558ed05707a9d7
Francis Fish <francis@dentally.co>
2017-10-10 17:08:40 +0100

    Envelope after transforms was breaking when prev transform turned node into a string

--- lib/xmldsig/transforms/enveloped_signature.rb
+++ lib/xmldsig/transforms/enveloped_signature.rb
@@ -2,6 +2,7 @@ module Xmldsig
   class Transforms < Array
     class EnvelopedSignature < Transform
       def transform
+        @node = Nokogiri::XML(node) if node.is_a? String
         signatures = node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).
             sort { |left, right| left.ancestors.size <=> right.ancestors.size }
 
